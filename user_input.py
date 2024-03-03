f977c647a6467d4c7447c2831c5898ae0e765d77
Ek-04 <125458901+Ek-04@users.noreply.github.com>
2024-03-03 17:17:42 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input ("Enter name:")
+age = eval(input("Enter age:"))
+location = input("Enter your location:")
+
+print(f"Hello  my name is {name} I am a {age}-year-old software developer living in {location}")
