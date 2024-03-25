afef5b65fb4159071b561e670c2b86e69eff1268
Ek-04 <125458901+Ek-04@users.noreply.github.com>
2024-03-25 22:55:03 +0300

    dart 2

--- controlStructures.dart
+++ controlStructures.dart
@@ -1,7 +1,26 @@
 import 'dart:io';
 
-Future<void> main() async {
-  stdout.write("Enter your number:");
-  String? n = stdin.readLineSync();
-  print(" you chose $n");
+Future<void> main() async
+{
+stdout.write("Enter your number: ");
+String? input =stdin.readLineSync();
+if (input != null)
+{
+  int n = int.tryParse(input) ?? 0; //parse strig to integer 
+
+
+
+  if (n < 10) 
+  {
+    print("your number si les than 10");
+  } 
+  else if (n > 10)
+   {
+    print("your number is greater than 10");
+  } 
+ else;
+  {
+    print("the number si equal to 10");
+  }
+}
 }
--- dummy.dart
+++ dummy.dart
@@ -12,50 +12,50 @@ void main() {
 }
 
 
-for n in range(n):
-    name  = input("Enter your name:")
-    age = input ("Enter your age:")
-    print('\n'.join([f"{name}: {age}" for name, age in name]))
+// for n in range(n):
+//     name  = input("Enter your name:")
+//     age = input ("Enter your age:")
+//     print('\n'.join([f"{name}: {age}" for name, age in name]))
 
 
 
 
 
     
-#user enters the list of integers 
-integers = input ("enter a list of  integers:")
+// #user enters the list of integers 
+// integers = input ("enter a list of  integers:")
 
-#the list is split into smaller strings ans stored in  integers_str
-integers_str = integers.split()
+// #the list is split into smaller strings ans stored in  integers_str
+// integers_str = integers.split()
 
-#the ssmall sttrigs are converted into integers and stored in integers_str_int
-integers_str_int = [int(num_str) for num_str in integers_str]
+// #the ssmall sttrigs are converted into integers and stored in integers_str_int
+// integers_str_int = [int(num_str) for num_str in integers_str]
 
-# the sum of integers is computed 
-sum = sum(integers_str_int)
+// # the sum of integers is computed 
+// sum = sum(integers_str_int)
 
-#the sum is printed 
-print (sum) 
-
-
+// #the sum is printed 
+// print (sum) 
 
 
 
-personal_info_list = []
-while True:
-    PERSONAL_INFO = {}
-name = input ("Enter your name:")
-
-age = input ("Enter yur age:")   
-
-if name.lower() == 'quit':
-   break 
 
 
-PERSONAL_INFO ['name '] = name
-PERSONAL_INFO ['age'] = age23 
-personal_info_list .append(PERSONAL_INFO)
+// personal_info_list = []
+// while True:
+//     PERSONAL_INFO = {}
+// name = input ("Enter your name:")
 
-print("people's information:")
-for index, info in enumerate(personal_info_list, start = 1):
- print(f"person{1}: {info}")
+// age = input ("Enter yur age:")   
+
+// if name.lower() == 'quit':
+//    break 
+
+
+// PERSONAL_INFO ['name '] = name
+// PERSONAL_INFO ['age'] = age23 
+// personal_info_list .append(PERSONAL_INFO)
+
+// print("people's information:")
+// for index, info in enumerate(personal_info_list, start = 1):
+//  print(f"person{1}: {info}")
